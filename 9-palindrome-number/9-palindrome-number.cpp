8428da4516caa43b6177b5d9a3b5c33eb465a27e
osama ibrahim <72796639+osama-ibrahim72@users.noreply.github.com>
2022-09-21 19:47:03 +0200

    Time: 27 ms (41.81%), Space: 5.9 MB (74.15%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+       
         if(x<0){
+             //the number has negative sign
             return 0;
         }
         if(x<10){
+            //the number has 1 digit
             return 1;
         }
+        //convert the number into string 
         string s;
         while(x){
             int tmp = x%10;
             s.push_back(tmp+'0');
             x/=10;
         }
+        
         for(int i=0;i<s.size()/2;i++){
+            //the number isn't Palindrome 
             if(s[i]!=s[s.size()-1-i])
                 return 0;
         }
+        //is Palindrome 
         return 1;
     }
 };
